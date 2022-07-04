939cbb30de4fb302197011228c4d2ad12b789b70
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-07-04 23:54:07 +0100

    feat: :sparkles: added CartItem component for the cart menu

---
+++ src/components/CartItem/CartItem.jsx
@@ -0,0 +1,31 @@
+import "./CartItem.css";
+
+const CartItem = ({ name, price, img, amount }) => {
+  return (
+    <div className="item__container">
+      <div>
+        <img
+          className="item__image"
+          src={require(`../../assets/img/products/${img}`)}
+          alt=""
+        />
+      </div>
+      <div className="item__info">
+        <p className="item__info_title info">{name}</p>
+        <p className="item__info_price info">€{price}</p>
+        <div className="item__bottom">
+          <input
+            className="item__bottom_input"
+            type="number"
+            name=""
+            value={amount}
+          />
+          <button className="item__bottom_button">Remove</button>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
