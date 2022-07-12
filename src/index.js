e48c1ddd0de331f2db013e041b26d096e2387e38
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-07-12 12:05:03 +0100

    feat: :sparkles: added routing

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { NextUIProvider } from "@nextui-org/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./components/Register/RegisterView/Register";
 
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <NextUIProvider>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+    {/* <App /> */}
   </NextUIProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
