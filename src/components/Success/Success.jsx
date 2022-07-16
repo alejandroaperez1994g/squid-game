262e93dc4435e41f479d1f18ed1d480d37c40ae9
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-07-16 22:30:59 +0100

    feat: :sparkles: added Succes component

---
+++ src/components/Success/Success.jsx
@@ -0,0 +1,19 @@
+import "./Succes.css";
+
+const Success = () => {
+  return (
+    <div className="success__container">
+      <div className="success__container_section">
+        <div className="success__container_top">
+          <p>Continue shopping</p>
+        </div>
+        <div className="success__section_wrapper">
+          <div className="left_section"></div>
+          <div className="right_section"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
