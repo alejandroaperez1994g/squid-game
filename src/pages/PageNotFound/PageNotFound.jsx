7afd53e018c9e7f7bb7270c24e7dfaa98a092bf5
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-07-29 19:25:57 +0200

    feat: :sparkles: set back to home button

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,6 +1,10 @@
+import { Button } from '@nextui-org/react';
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+import { useNavigate } from 'react-router-dom';
 import './PageNotFound.css';
 
 const PageNotFound = () => {
+  const navigator = useNavigate();
   return (
     <div className="main__container">
       <div className="section__404">
@@ -19,6 +23,15 @@ const PageNotFound = () => {
           src={require('../../assets/img/404/page-not-found.png')}
           alt="404"
         />
+        <Button
+          className="sharp_font"
+          color="secondary"
+          shadow
+          icon={<KeyboardDoubleArrowLeftIcon />}
+          onClick={() => navigator('/')}
+        >
+          Return to Home
+        </Button>
       </div>
     </div>
   );
