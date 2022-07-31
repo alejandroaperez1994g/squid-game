5f8c03f751482ac71618d8fce0c4e4eaafac4b73
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-07-31 03:09:17 +0200

    refactor: :recycle: Refactored login page behavior, added sign up with firebase

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,55 +1,13 @@
-import { useContext, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
-import { Input, Button } from '@nextui-org/react';
-import { signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth';
-import { auth, provider } from '../../firebase';
-import { useNavigate } from 'react-router-dom';
-import { UserContext } from '../../contexts/UserContext';
-import { CartContext } from '../../contexts/CartContext';
+import { Button } from '@nextui-org/react';
 
+import { useToggle, useAuth } from '../../hooks';
+import { SignInForm, SignUpForm } from '../../components';
 import './Login.css';
 
 const Login = () => {
-  const navigator = useNavigate();
-  const { setUserData } = useContext(UserContext);
-  const { notify } = useContext(CartContext);
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const resetForm = () => {
-    setPassword('');
-    setEmail('');
-  };
-
-  const handleSignInWithPopup = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const { displayName, email, photoURL } = result.user;
-        setUserData({ name: displayName, email: email, photo: photoURL });
-        navigator('/');
-      })
-      .catch((error) => {
-        console.error(error.code);
-        console.error(error.message);
-        notify(`${error.message}`, 'error');
-      });
-  };
-
-  const handleSignInWithEmail = () => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((result) => {
-        const { displayName, email, photoURL } = result.user;
-        setUserData({ name: displayName, email: email, photo: photoURL });
-        navigator('/');
-      })
-      .catch((error) => {
-        console.error(error.code);
-        console.error(error.message);
-        resetForm();
-        notify(`${error.message}`, 'error');
-      });
-  };
+  const siRegistering = useToggle();
+  const { handleSignInWithPopup } = useAuth();
 
   return (
     <div className="login__container">
@@ -67,41 +25,38 @@ const Login = () => {
             <p className="top__section_description">
               Welcome back! Please enter your details.
             </p>
-            <form className="section__form" action="" method="">
-              <Input
-                labelPlaceholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <Input.Password
-                labelPlaceholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <p className="forgotPassword">Forgot password?</p>
-              <Button color="secondary" onClick={handleSignInWithEmail}>
-                Sign In
-              </Button>
+            {siRegistering.state ? <SignUpForm /> : <SignInForm />}
+            <Button
+              ghost
+              icon={
+                <img
+                  className="google_icon"
+                  src={require('../../assets/img/google.png')}
+                  alt="google"
+                />
+              }
+              css={{
+                width: '100%',
+                background: 'white',
+                color: 'black',
+                border: '1px solid gray',
+              }}
+              onClick={handleSignInWithPopup}
+            >
+              Sign in with Google
+            </Button>
+            <p className="signUp_text">
+              Don't have an account?
               <Button
-                ghost
-                icon={
-                  <img
-                    className="google_icon"
-                    src={require('../../assets/img/google.png')}
-                    alt="google"
-                  />
-                }
-                css={{
-                  background: 'white',
-                  color: 'black',
-                  border: '1px solid gray',
-                }}
-                onClick={handleSignInWithPopup}
+                light
+                color="secondary"
+                auto
+                size="md"
+                onClick={siRegistering.onToggle}
               >
-                Sign in with Google
+                {siRegistering.state ? 'Sign in' : 'Sign up'}
               </Button>
-            </form>
-            <p className="signUp_text">Don't have an account? Sign up.</p>
+            </p>
           </div>
         </div>
         <div className="right_section "></div>
