2c3b03686a82377b747a5226e16dc5d474dd8326
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-08-09 01:23:33 +0100

    feat: :sparkles: upload local changes in shopping Cart to firestore

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,17 +1,33 @@
-import { createContext, useState, useEffect } from 'react';
-import { UserContextProvider } from './UserContext';
-import { WishContextProvider } from './WishContext';
+import { createContext, useState, useEffect, useContext } from 'react';
+
 import toast from 'react-hot-toast';
+import { UserContext } from './UserContext';
+import { db } from '../firebase';
+import { updateDoc, doc } from 'firebase/firestore';
 
 export const CartContext = createContext();
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 export const CartContextProvider = ({ children }) => {
   const [shoppingCart, setShoppingCart] = useState(cart);
+  const { userData } = useContext(UserContext);
+
+  const uploadShoppingCart = async (user, shoppingCart) => {
+    try {
+      const userDoc = doc(db, 'squid-store-users', user);
+      const newField = { shopping_cart: shoppingCart };
+      await updateDoc(userDoc, newField);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(shoppingCart));
-  }, [shoppingCart]);
+    if (userData) {
+      uploadShoppingCart(userData.email, shoppingCart);
+    }
+  }, [shoppingCart, userData]);
 
   const notify = (message, type = 'success') => {
     if (type === 'error') {
@@ -23,9 +39,7 @@ export const CartContextProvider = ({ children }) => {
 
   return (
     <CartContext.Provider value={{ shoppingCart, setShoppingCart, notify }}>
-      <UserContextProvider>
-        <WishContextProvider>{children}</WishContextProvider>
-      </UserContextProvider>
+      {children}
     </CartContext.Provider>
   );
 };
